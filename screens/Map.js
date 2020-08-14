6aa1b4b6c64b9aef4be05ca5977b281ec498cb17
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2020-08-13 21:11:44 -0400

    added data for parking

--- screens/Map.js
+++ screens/Map.js
@@ -76,3 +76,29 @@ const styles = StyleSheet.create({
     }
 });
 
+const parkingsSpots = [
+    {
+      id: 1,
+      title: 'Parking 1',
+      price: 5,
+      rating: 4.2,
+      spots: 20,
+      free: 10
+    },
+    {
+      id: 2,
+      title: 'Parking 2',
+      price: 7,
+      rating: 3.8,
+      spots: 25,
+      free: 20
+    },
+    {
+      id: 3,
+      title: 'Parking 3',
+      price: 10,
+      rating: 4.9,
+      spots: 50,
+      free: 25
+    },
+  ];
