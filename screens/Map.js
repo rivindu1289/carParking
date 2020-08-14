f18c6ca91530bf4414b8a51d288b2c2922e52d72
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2020-08-14 18:38:09 -0400

    added icons and changed scroll to flatlist

--- screens/Map.js
+++ screens/Map.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, Dimensions, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Dimensions, TouchableOpacity } from 'react-native';
 import MapView from 'react-native-maps';
+import { FontAwesome, Ionicons } from '@expo/vector-icons';
+
 
 export default class Map extends React.Component {
     state = {
@@ -38,41 +40,51 @@ export default class Map extends React.Component {
             <View key={`parking-${item.id}`} style={styles.parking}>
                 <View style={{ flex: 1, flexDirection: 'column' }}>
                     <Text style={{ fontSize: 16 }}>x {item.spots} {item.title}</Text>
-                </View>
-                <View style={{ flex: 1, flexDirection: 'row' }}>
-                    <View style={{ flex: 1, justifyContent: 'center' }}>
-                        <Text>${item.price}</Text>
-                        <Text>{item.rating}</Text>
+                    <View style={{ width: 100, borderRadius: 6, borderColor: 'grey', borderWidth: 0.7, padding: 4 }}>
+                        <Text style={{ fontSize: 16 }}>05:00 hrs</Text>
                     </View>
-                    <TouchableWithoutFeedback>
-                        <View style={styles.buy}>
-                            <View style={{ flex: 1, justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 25, color: 'white' }}>${item.price * 2}</Text>
-                                <Text style={{ color: 'white' }}>{item.price}x{hours[item.id]} hrs</Text>
-                            </View>
-                            <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }}>
-                                <Text style={{ fontSize: 25, color: 'white' }}>></Text>
-                            </View>
+                </View>
+                <View style={{ flex: 1.5, flexDirection: 'row' }}>
+                    <View style={{ flex: 0.5, justifyContent: 'center', marginHorizontal: 24 }}>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Ionicons name='ios-pricetag' size={16} color="#70818A" />
+                            <Text>${item.price}</Text>
                         </View>
-                    </TouchableWithoutFeedback>
+                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+                            <Ionicons name='ios-star' size={16} color="#70818A" />
+                            <Text>{item.rating}</Text>
+                        </View>
+                    </View>
+                    <TouchableOpacity style={styles.buy}>
+                        <View style={{ flex: 1, justifyContent: 'center' }}>
+                            <Text style={{ fontSize: 25, color: 'white' }}>${item.price * 2}</Text>
+                            <Text style={{ color: 'white' }}>{item.price}x{hours[item.id]} hrs</Text>
+                        </View>
+                        <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }}>
+                            <Text style={{ fontSize: 25, color: 'white' }}>{">"}</Text>
+                        </View>
+                    </TouchableOpacity>
                 </View>
             </View>
         )
     }
 
-    renderParkings() {
-        return (
-            <ScrollView horizontal
-                style={styles.parkings}
-                pagingEnabled
-                showsHorizontalScrollIndicator={false}
-                scrollEventThrottle={16}
-                snapToAlignment="center"
-            >
-                {parkingsSpots.map(parking => this.renderParking(parking))}
-            </ScrollView>
+    renderParkings(){
+        return(
+          <FlatList
+            horizontal
+            pagingEnabled
+            scrollEnabled
+            showsHorizontalScrollIndicator={false}
+            scrollEventThrottle={16}
+            snapToAlignment="center"
+            style={styles.parkings}
+            data={parkingsSpots}
+            keyExtractor={(item, index) => `${item.id}`}
+            renderItem={({ item }) => this.renderParking(item)}
+          />
         )
-    }
+  }
 }
 
 const w = Dimensions.get('screen').width;
@@ -102,16 +114,16 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         backgroundColor: 'white',
         borderRadius: 6,
-        padding: 20,
+        padding: 15,
         marginHorizontal: 24,
         width: w - 48
     },
     buy: {
-        flex: 1,
+        flex: 1.25,
         flexDirection: 'row',
         padding: 8,
         borderRadius: 6,
-        backgroundColor: 'red'
+        backgroundColor: '#D83C54'
     },
 });
 
