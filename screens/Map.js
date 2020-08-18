9639fe2a735795d046ca48e3f060a5efd46b67ab
rivindu1289 <44213720+rivindu1289@users.noreply.github.com>
2020-08-18 12:03:11 -0400

    Updated colors to use theme.js

--- screens/Map.js
+++ screens/Map.js
@@ -10,6 +10,7 @@ import {
 } from 'react-native';
 import MapView from 'react-native-maps';
 import { FontAwesome, Ionicons } from '@expo/vector-icons';
+import * as theme from '../theme';
 
 const { Marker } = MapView;
 
@@ -32,20 +33,20 @@ export default class Map extends React.Component {
                     }}
                 >
                     {parkingsSpots.map(parking =>
-                        <Marker 
-                        key={`marker-${parking.id}`}
-                        coordinate={parking.coordinate}>
-                          <TouchableWithoutFeedback onPress={() => this.setState({ active: parking.id })} >
-                            <View style={[
-                              styles.marker,
-                              styles.shadow,
-                              this.state.active === parking.id ? styles.active : null
-                            ]}>
-                              <Text style={styles.markerPrice}>${parking.price}</Text>
-                              <Text style={styles.markerStatus}> ({parking.free}/{parking.spots})</Text>
-                            </View>
-                          </TouchableWithoutFeedback>
-                      </Marker>
+                        <Marker
+                            key={`marker-${parking.id}`}
+                            coordinate={parking.coordinate}>
+                            <TouchableWithoutFeedback onPress={() => this.setState({ active: parking.id })} >
+                                <View style={[
+                                    styles.marker,
+                                    styles.shadow,
+                                    this.state.active === parking.id ? styles.active : null
+                                ]}>
+                                    <Text style={styles.markerPrice}>${parking.price}</Text>
+                                    <Text style={styles.markerStatus}> ({parking.free}/{parking.spots})</Text>
+                                </View>
+                            </TouchableWithoutFeedback>
+                        </Marker>
                     )}
                 </MapView>
                 {this.renderParkings()}
@@ -68,28 +69,28 @@ export default class Map extends React.Component {
                 <View style={[styles.parking, styles.shadow]}>
                     <View style={{ flex: 1, flexDirection: 'column' }}>
                         <Text style={{ fontSize: 16 }}>x {item.spots} {item.title}</Text>
-                        <View style={{ width: 100, borderRadius: 6, borderColor: 'grey', borderWidth: 0.7, padding: 4 }}>
+                        <View style={{ width: 100, borderRadius: 6, borderColor: theme.COLORS.gray, borderWidth: 0.7, padding: 4 }}>
                             <Text style={{ fontSize: 16 }}>05:00 hrs</Text>
                         </View>
                     </View>
                     <View style={{ flex: 1.5, flexDirection: 'row' }}>
                         <View style={{ flex: 0.5, justifyContent: 'center', marginHorizontal: 24 }}>
                             <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                                <Ionicons name='ios-pricetag' size={16} color="#70818A" />
+                                <Ionicons name='ios-pricetag' size={16} color={theme.COLORS.gray} />
                                 <Text>${item.price}</Text>
                             </View>
                             <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-                                <Ionicons name='ios-star' size={16} color="#70818A" />
+                                <Ionicons name='ios-star' size={16} color={theme.COLORS.gray} />
                                 <Text>{item.rating}</Text>
                             </View>
                         </View>
                         <TouchableOpacity style={styles.buy}>
                             <View style={{ flex: 1, justifyContent: 'center' }}>
-                                <Text style={{ fontSize: 25, color: 'white' }}>${item.price * 2}</Text>
-                                <Text style={{ color: 'white' }}>{item.price}x{hours[item.id]} hrs</Text>
+                                <Text style={{ fontSize: 25, color: theme.COLORS.white }}>${item.price * 2}</Text>
+                                <Text style={{ color: theme.COLORS.white }}>{item.price}x{hours[item.id]} hrs</Text>
                             </View>
                             <View style={{ flex: 0.5, justifyContent: 'center', alignItems: 'center' }}>
-                                <Text style={{ fontSize: 25, color: 'white' }}>></Text>
+                                <Text style={{ fontSize: 25, color: theme.COLORS.white }}>></Text>
                             </View>
                         </TouchableOpacity>
                     </View>
@@ -127,9 +128,7 @@ const w = Dimensions.get('screen').width;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
+        backgroundColor: theme.COLORS.white
     },
     map: {
         flex: 3,
@@ -139,16 +138,16 @@ const styles = StyleSheet.create({
         flex: .5,
         justifyContent: 'center',
     },
-    parkings :{
+    parkings: {
         position: 'absolute',
         right: 0,
         left: 0,
         bottom: 24,
-        paddingBottom : 24
-      },
+        paddingBottom: 24
+    },
     parking: {
         flexDirection: 'row',
-        backgroundColor: 'white',
+        backgroundColor: theme.COLORS.white,
         borderRadius: 6,
         padding: 15,
         marginHorizontal: 24,
@@ -159,37 +158,37 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         padding: 8,
         borderRadius: 6,
-        backgroundColor: '#D83C54'
+        backgroundColor: theme.COLORS.red
     },
     marker: {
         flexDirection: 'row',
-        backgroundColor: '#FFFFFF',
+        backgroundColor: theme.COLORS.white,
         borderRadius: 24,
         paddingVertical: 12,
         paddingHorizontal: 24,
         borderWidth: 1,
-        borderColor: '#FFFFFF',
+        borderColor: theme.COLORS.white,
     },
     markerPrice: {
-        color: '#D83C54',
+        color: theme.COLORS.red,
         fontWeight: 'bold',
     },
     markerStatus: {
-        color: '#7D818A'
+        color: theme.COLORS.gray
     },
     shadow: {
-        shadowColor: "#3D4448",
+        shadowColor: theme.COLORS.black,
         shadowOffset: {
             width: 0,
             height: 6,
         },
         shadowOpacity: 0.1,
         shadowRadius: 4,
-        backgroundColor: 'white',
-        elevation: 5
+        backgroundColor: theme.COLORS.white,
+        elevation: 15
     },
     active: {
-        borderColor: '#D83C54',
+        borderColor: theme.COLORS.red,
     },
 });
 
